ac6324528294c4053c29466aca05c833d5143745
VanTho15 <nguyenvanthochutsinivesoss15@gmail.com>
2020-07-22 22:55:50 +0700

    tho add

--- 1.js
+++ 1.js
@@ -1,4 +1,19 @@
 ﻿ $(function(){
- 
+   vitricuaedu=$('#khoiedu').offset().top -96;
+   vitrisanpham=$('#khoisanpham').offset().top -96;
+   console.log(vitricuaedu);
+
+ 	$(".xuong,.kn").click(function()
+ 	{
+ 		$('body,html').animate({scrollTop:vitricuaedu},700);
+ 		return false;
+ 	})
+
+
+ 	$(".sp").click(function()
+ 	{
+ 		$('body,html').animate({scrollTop:vitrisanpham},700);
+ 		return false;
+ 	})
 })  
  
